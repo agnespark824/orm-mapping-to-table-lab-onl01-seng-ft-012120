b38bc6d90cc04af43df283d1fded98e4c1cf3716
Agnes Park <agnespark824@gmail.com>
2020-02-28 05:30:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,16 +14,33 @@ class Student
       name TEXT,
       grade TEXT,
       id INTEGER PRIMARY KEY
-      );
+      )
       SQL
+   
     DB[:conn].execute(sql)
   end
   
-    def self.drop_table
+  def self.drop_table
     sql = <<-SQL #leading tabs are ignored
-    DROP TABLE students;
+    DROP TABLE students
       SQL
+   
     DB[:conn].execute(sql)
+  end
+  
+  def save
+    sql = <<-SQL
+    INSERT INTO students (name, grade, id)
+      VALUES (?, ?, ?)
+    SQL
+    
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+  
+  def self.create(:name, :grade)
+    student = Student.new(:name, :grade)
+    student.save
+    student
   end
   
 end
