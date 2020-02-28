c0c37015d5c07447303616e762fc941cc76e3767
Agnes Park <agnespark824@gmail.com>
2020-02-28 04:45:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,16 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+  
+  def create_table
+    CREATE TABLE students (name, grade, id)
+    
+  end
   
 end
